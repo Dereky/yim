719750343aaad4dca3101ae6db2557e804920426
dereky <dereky@gmail.com>
2011-05-01 18:43:26 -0400

    changed homepage

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yim::Application.config.secret_token = '6b406f1d178bbfecde8195f5a8aee79d41419098fff7532495910d96e13434fad62716e641faa4e14c60669ca80a25a77de32fa94b1a530fd27ecb1b18aeea91'
